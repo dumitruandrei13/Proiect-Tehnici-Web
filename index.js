a21756dbb92f5dc31c7c56d04357d55fe3017ecc
dumitruandrei13 <andreidragos1309@gmail.com>
2026-05-03 18:40:16 +0300

    Etapa 4

--- index.js
+++ index.js
@@ -1,54 +1,89 @@
+const fs = require("fs");
 const express= require("express");
 const path= require("path");
 
 app= express();
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+
+const vect_foldere = ["temp", "logs", "backup", "fisiere_uploadate"];
+
+for (let folder of vect_foldere) {
+    let caleFolder = path.join(__dirname, folder);
+    if (!fs.existsSync(caleFolder)) {
+        fs.mkdirSync(caleFolder);
+    }
+}
+
+const obGlobal = {
+    obErori: null
+};
+
+function initErori() {
+    const caleJSON = path.join(__dirname, "resurse/json/erori.json");
+    const dateFisier = fs.readFileSync(caleJSON, "utf8");
+    obGlobal.obErori = JSON.parse(dateFisier);
+
+    // Setăm căile "absolute" (de la rădăcina serverului) pentru imagini
+    obGlobal.obErori.eroare_default.imagine = path.posix.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine);
+
+    for (let eroare of obGlobal.obErori.info_erori) {
+        eroare.imagine = path.posix.join(obGlobal.obErori.cale_baza, eroare.imagine);
+    }
+}
+
+initErori();
+
+function afisareEroare(res, identificator, titlu, text, imagine) {
+    let eroare = obGlobal.obErori.info_erori.find(e => e.identificator == identificator);
+    if (!eroare) eroare = obGlobal.obErori.eroare_default;
+
+    let titluAfisat = titlu || eroare.titlu;
+    let textAfisat = text || eroare.text;
+    let imagineAfisata = imagine ? path.posix.join(obGlobal.obErori.cale_baza, imagine) : eroare.imagine;
+
+    let status_raspuns = (eroare.status && identificator) ? identificator : 200;
+
+    res.status(status_raspuns).render("pagini/eroare", {
+        titlu: titluAfisat,
+        text: textAfisat,
+        imagine: imagineAfisata
+    });
+}
 
 console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
 console.log("Cale fisier", __filename);
 
-app.get("/cale",function(req,res){
-    console.log("Am primit o cerere GET la adresa /cale");
-    res.send("Raspuns la <b style='color:red;'>cererea </b> GET la adresa /cale.");
-});
-
-
-app.get("/cale/:a/:b",function(req,res){
-  res.send(parseInt(req.params.a) + parseInt(req.params.b));
-});
-
-app.get("/:a/:b",function(req,res){
-  res.sendFile(path.join(__dirname,"index.html"));
-});
-
-
-
-
-// app.get("/resurse/css/general.css",function(req,res){
-//     res.sendFile(path.join(__dirname,"resurse/css/general.css"));
-// });
-
-
 app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
-app.get("/cale2",function(req,res){
-    res.write("ceva ");
-    res.write("altceva");
-    res.end();
+app.get(/^\/resurse\/(.*)/, (req, res) =>{
+    afisareEroare(res, 403);
 });
 
-
-app.get("/despre",function(req,res){
-res.render("pagini/despre")
+app.get(/\.ejs$/, function(req, res) {
+    afisareEroare(res, 400);
 });
 
-app.get("/", function(req, res) {
+app.get("/favicon.ico", function(req, res) {
+    res.sendFile(path.join(__dirname, "resurse/imagini/favicon/favicon.ico"));
+});
+
+app.get(["/", "/index", "/home"], function(req, res) {
     res.render("pagini/index");
 });
 
-app.get("/index", function(req, res) {
-    res.render("pagini/index");
+app.get(/^(.*)$/, function(req, res) {
+    res.render("pagini" + req.url, function(err, rezHtml) {
+        if (err) {
+            if (err.message.startsWith("Failed to lookup view")) {
+                afisareEroare(res, 404);
+            } else {
+                afisareEroare(res, 500);
+            }
+        } else {
+            res.send(rezHtml);
+        }
+    });
 });
 
 app.listen(8080);
